8776d9a05c755156a80ff1eefb469082654ae7a7
Cristiano <cristiano.sushy@hotmail.com>
2022-12-08 00:07:27 -0300

    Atualizado

--- src/Matriz.java
+++ src/Matriz.java
@@ -1,5 +1,35 @@
-public class Main {
+import java.util.Scanner;
+
+public class Matriz {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] mat = new int[n][n];
+
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[i].length; j++){
+                mat[i][j] = sc.nextInt();
+
+            }
+        }
+
+        System.out.println("Diagonal Principal: ");
+        for(int i = 0; i < mat.length; i++){
+            System.out.print(mat[i][i]+ " ");
+
+        }
+        System.out.println();
+
+        int count = 0;
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[i].length; j++){
+                if(mat[i][j] < 0){
+                    count++;
+                }
+            }
+        }
+        System.out.println("Numeros Negativos: " + count);
+        sc.close();
     }
 }
